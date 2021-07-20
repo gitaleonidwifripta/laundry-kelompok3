d7ecffd3185eaa0fcbc484dfbe9990190a1818cb
gitaleonidwifripta <80527607+gitaleonidwifripta@users.noreply.github.com>
2021-07-21 00:24:01 +0700

    Add files via upload

---
+++ src/laundry/s.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laundry;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author USER
+ */
+class s {
+
+    static String format(Date ys) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static String format(java.util.Date ys) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
